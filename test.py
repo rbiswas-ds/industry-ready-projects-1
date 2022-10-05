da0d9300fc0055eb97ff889907997f2fc93af75d
rakeshbsw2022 <rakeshbsw2022@gmail.com>
2022-10-05 18:35:18 +0530

    test

--- test.py
+++ test.py
@@ -2,10 +2,7 @@ print("My test python script")
 print("My first commit in developer branch")
 print("This is my commit in  the dev2 branch")
 print("This is my dev3 branch")
-<<<<<<< HEAD
-print("This is my commit in  main branch of original repository")
-=======
+
 print("This is  my Fork project")
 print("This is  my own Forked Repo")
->>>>>>> fd25e1f4a55b07eed26f29c52d5a6cfc28679e3b
 print("This is my Fork1 branch")
